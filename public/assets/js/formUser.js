ae0338eee4000368c7eddf9cacf0ad3c7ee1face
Enayat Badru <Enayatbadru07@gmail.com>
2023-10-07 21:18:41 +0200

    my first commit

---
+++ public/assets/js/formUser.js
@@ -0,0 +1,36 @@
+const rightSvgTop = document.querySelectorAll('.right-svg-top');
+const password = document.querySelector('#password');
+const rightSvgBottom = document.querySelectorAll('.right-svg-bottom');
+const confirmPassword = document.querySelector('#conf-password');
+
+rightSvgTop.forEach((element) => {
+    element.addEventListener('click', () => {
+        const visible = element.querySelector('.visible');
+        const hidden = element.querySelector('.hidden');
+
+        visible.classList.toggle('active');
+        hidden.classList.toggle('active');
+
+        if (visible.classList.contains('active')) {
+            password.type = 'text';
+        } else {
+            password.type = 'password';
+        }
+    });
+});
+
+rightSvgBottom.forEach((element) => {
+    element.addEventListener('click', () => {
+        const visible = element.querySelector('.visible');
+        const hidden = element.querySelector('.hidden');
+
+        visible.classList.toggle('active');
+        hidden.classList.toggle('active');
+
+        if (visible.classList.contains('active')) {
+            confirmPassword.type = 'text';
+        } else {
+            confirmPassword.type = 'password';
+        }
+    });
+});
